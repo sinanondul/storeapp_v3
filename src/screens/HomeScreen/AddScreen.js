8c44aa7f52632a7390c46e6ab8b6317d49f37bbf
sinanondul <sinanondul@gmail.com>
2021-01-28 02:45:46 +0300

    lebron added to home-addscreen

--- src/screens/HomeScreen/AddScreen.js
+++ src/screens/HomeScreen/AddScreen.js
@@ -1,37 +1,73 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  SafeAreaView,
+  Image,
+} from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+  },
+  headerTitleStyle: {
+    flex: 0.6,
+    paddingRight: 60,
+    alignSelf: "center",
     alignItems: "center",
-    // textAlign: "center",
+    fontWeight: "bold",
+  },
+  inputContainer: {
+    margin: 32,
+    flexDirection: "row",
+  },
+  avatar: {
+    width: 40,
+    height: 48,
+    borderRadius: 24,
+    marginRight: 18,
+  },
+  photo: {
+    alignItems: "flex-end",
+    marginHorizontal: 32,
   },
 });
 
 class AddScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Add Posts Now</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Image
+            source={require("../../../assets/lebron.png")}
+            style={styles.avatar}
+          ></Image>
+          <TextInput
+            outerFocus={true}
+            multiline={true}
+            numberOfLines={4}
+            style={{ flex: 1 }}
+            placeholder="Type Here"
+          ></TextInput>
+        </View>
+        <TouchableOpacity style={styles.photo}>
+          <Ionicons name="md-camera" size={32} color="black"></Ionicons>
+        </TouchableOpacity>
+      </SafeAreaView>
     );
   }
-
+  //
   static navigationOptions = {
     title: <Text>Supplies</Text>,
     headerStyle: {
       backgroundColor: "#2890cf",
     },
     headerTintColor: "#fff",
-    headerTitleStyle: {
-      flex: 0.6,
-      paddingRight: 60,
-      alignSelf: "center",
-      alignItems: "center",
-      fontWeight: "bold",
-    },
   };
 }
 
