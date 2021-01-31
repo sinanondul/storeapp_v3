ea5a822e9fe0eef6f012a22283d5f166cba8c33e
SimonYuvarlak <simonyuvarlak@icloud.com>
2021-01-31 14:17:25 +0300

    profile page template committed

--- src/screens/ProfileScreen/LandingScreen.js
+++ src/screens/ProfileScreen/LandingScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import {View, Platform, Text} from "react-native";
+import {View, Platform, Text, SafeAreaView, ScrollView, Image} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { openDrawer } from "../../../App";
 import styles from "./styles";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
   
 export default class LandingScreen extends React.Component{
     
@@ -17,11 +18,11 @@ export default class LandingScreen extends React.Component{
 
         headerRight: () => (
           <Icon
-            name="ios-add-circle-outline"
+            name={Platform.OS === "ios" ? "ios-settings" : "md-settings"}
             style={{marginRight:10}}
             size={40}
             color='#fff'
-            onPress={() => this.props.navigation.navigate("Add")}
+            onPress={() => this.props.navigation.navigate("Settings")}
           />
         )
       });
@@ -30,12 +31,82 @@ export default class LandingScreen extends React.Component{
     render(){
       
       return (
-        
-        <View 
-        style={styles.container}>
-            <Text>Profile page.</Text>
-        </View>
-      );
+        <SafeAreaView style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+
+                <View style={{ alignSelf: "center" }}>
+                    <View style={styles.profileImage}>
+                        <Image source={require("./assets/profile-pic.jpg")} style={styles.image} resizeMode="center"></Image>
+                    </View>
+                    <View style={styles.dm}>
+                        <MaterialIcons name="chat" size={18} color="#DFD8C8"></MaterialIcons>
+                    </View>
+                    <View style={styles.active}></View>
+                    <View style={styles.add}>
+                        <Ionicons name="ios-add" size={48} color="#DFD8C8" style={{ marginTop: 6, marginLeft: 2 }}></Ionicons>
+                    </View>
+                </View>
+
+                <View style={styles.infoContainer}>
+                    <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>Julie</Text>
+                    <Text style={[styles.text, { color: "#AEB5BC", fontSize: 14 }]}>Photographer</Text>
+                </View>
+
+                <View style={styles.statsContainer}>
+                    <View style={styles.statsBox}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>483</Text>
+                        <Text style={[styles.text, styles.subText]}>Posts</Text>
+                    </View>
+                    <View style={[styles.statsBox, { borderColor: "#DFD8C8", borderLeftWidth: 1, borderRightWidth: 1 }]}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>45,844</Text>
+                        <Text style={[styles.text, styles.subText]}>Followers</Text>
+                    </View>
+                    <View style={styles.statsBox}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>302</Text>
+                        <Text style={[styles.text, styles.subText]}>Following</Text>
+                    </View>
+                </View>
+
+                <View style={{ marginTop: 32 }}>
+                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("./assets/media1.jpg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("./assets/media2.jpg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("./assets/media3.jpg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                    </ScrollView>
+                    <View style={styles.mediaCount}>
+                        <Text style={[styles.text, { fontSize: 24, color: "#DFD8C8", fontWeight: "300" }]}>70</Text>
+                        <Text style={[styles.text, { fontSize: 12, color: "#DFD8C8", textTransform: "uppercase" }]}>Media</Text>
+                    </View>
+                </View>
+                <Text style={[styles.subText, styles.recent]}>Recent Activity</Text>
+                <View style={{ alignItems: "center" }}>
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                Started following <Text style={{ fontWeight: "400" }}>Jake Challeahe</Text> and <Text style={{ fontWeight: "400" }}>Luis Poteer</Text>
+                            </Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                Started following <Text style={{ fontWeight: "400" }}>Luke Harper</Text>
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
     }
   
     static navigationOptions = {
