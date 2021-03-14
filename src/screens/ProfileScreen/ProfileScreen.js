ccc1d22253ac67802a087ccac03e1011af248ca2
sinanondul <sinanondul@gmail.com>
2021-03-14 13:45:23 +0300

    profile work merged //TODO

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -5,36 +5,49 @@ import LandingScreen from "./LandingScreen";
 import MessagingInterface from "../MessagesScreen/MessagingInterface";
 //import AddScreen from "./AddScreen";
 import styles from "./styles";
-import { Alert } from 'react-native';
+import { Alert } from "react-native";
 
-export default class ProfileScreen extends React.Component{
+export default class ProfileScreen extends React.Component {
+  componentDidMount() {}
 
+  componentWillUnmount() {}
 
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-    
-  render(){
+  render() {
     const ProfileStack = createStackNavigator();
     var userInfo = this.props.userData;
-    if (this.props.route.params)
-    {
-      if (this.props.route.params.userInfo)
-      {
+    if (this.props.route.params) {
+      if (this.props.route.params.userInfo) {
         userInfo = this.props.route.params.userInfo;
       }
     }
     return (
-      <ProfileStack.Navigator
-        initialRouteName='Landing'
-      >
-        <ProfileStack.Screen name="Landing" options={LandingScreen.navigationOptions}>
-          {(props) => <LandingScreen {...this.props} {...props} userData={this.props.userData} userInfo={userInfo}/>}
+      <ProfileStack.Navigator initialRouteName="Landing">
+        <ProfileStack.Screen
+          name="Landing"
+          options={LandingScreen.navigationOptions}
+        >
+          {(props) => (
+            <LandingScreen
+              {...this.props}
+              {...props}
+              userData={this.props.userData}
+              userInfo={userInfo}
+            />
+          )}
         </ProfileStack.Screen>
-        <ProfileStack.Screen name="MessagingFromProfile" options={MessagingInterface.navigationOptions}>
-          {(props) => <MessagingInterface {...this.props} {...props}/>}
+        <ProfileStack.Screen
+          name="EditProfile"
+          options={LandingScreen.navigationOptions}
+        >
+          {(props) => (
+            <EditProfileScreen {...props} userData={this.props.userData} />
+          )}
+        </ProfileStack.Screen>
+        <ProfileStack.Screen
+          name="MessagingFromProfile"
+          options={MessagingInterface.navigationOptions}
+        >
+          {(props) => <MessagingInterface {...this.props} {...props} />}
         </ProfileStack.Screen>
       </ProfileStack.Navigator>
     );
