08f86ab042c560a51f8c04db911f71f7824d92b4
ufukturker <76185343+ufukturker@users.noreply.github.com>
2021-04-25 15:11:50 +0300

    notificationItem_init

---
+++ src/screens/NotificationsScreen/components/NotificationItem.js
@@ -0,0 +1,34 @@
+
+const multiNotificationItem = {
+    lastSender: {
+        avatar: null,
+        name: "tempuser",
+        fullName: "tempUser x",
+    },
+    targetInfo: {
+        type: "post",
+        action: "up",
+        postId: null,
+    },
+    new: false,
+    senderCount: 3,
+    timestamp: 1619347662400,
+
+}
+
+const singleNotificationItem = {
+    lastSender: {
+        avatar: null,
+        name: "tempuser",
+        fullName: "tempUser x",
+    },
+    targetInfo: {
+        type: "post",
+        action: "comment",
+        postId: null,
+        text: "A comment asdjfkjasdflasdlkfjaslkdj fasjdlkfaljksdnfjkansdjkfnakjsdnfkjasnkdjfnajhsdgvcsoakpoej rbahıdsbcıans0eısadfadfmasdklfmlkasdmfamsldkf mlaksdmfasmdlkfmaslkdfmlasd",
+    },
+    senderCount: 1,
+    new: true,
+    timestamp: 1619347662401,
+}
